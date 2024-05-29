6eb7c807ab894cd1cd677ef0d393e937e3c466df
khamitovam <azamat1008@mail.ru>
2024-05-29 21:41:21 +0400

    Инициализация ДЗ 4

---
+++ hw4/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -0,0 +1,39 @@
+package ru.otus.hw.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Locale;
+import java.util.Map;
+
+
+@ConfigurationProperties(prefix = "application")
+@Setter
+// Использовать @ConfigurationProperties.
+// Сейчас класс соответствует файлу настроек. Чтобы они сюда отобразились нужно только правильно разместить аннотации
+public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
+
+    @Getter
+    private int rightAnswersCountToPass;
+
+    @Getter
+    private Locale locale;
+
+    private Map<Locale, String> fileNameByLocaleTag;
+
+    @Override
+    public int getRightAnswersCountToPass() {
+        return rightAnswersCountToPass;
+    }
+
+    @Override
+    public String getTestFileName() {
+        return fileNameByLocaleTag.get(locale);
+    }
+
+    @Override
+    public Locale getLocale() {
+        return locale;
+    }
+}
---
+++ hw4/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,43 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLine("");
+
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+        int answerNumber;
+
+        for (var question: questions) {
+            ioService.printFormattedLine(ioService.getMessage("TestService.question"),question.text());
+            for (Answer answer : question.answers()) {
+                ioService.printFormattedLine(ioService.getMessage("TestService.answer")
+                        ,question.answers().indexOf(answer) + 1,answer.text());
+            }
+            answerNumber = ioService.readIntForRange(1,question.answers().size()
+                    , ioService.getMessage("TestService.answer.check.number",question.answers().size()));
+            testResult.applyAnswer(question, question.answers().get(answerNumber - 1).isCorrect());
+            ioService.printLine("");
+        }
+
+        return testResult;
+    }
+
+}
